4c8cc5e51cc0038f1d6b6a8757d5b4f96185bea5
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-09-20 13:22:50 -0500

    working group add to db

--- www/templates/group/group.js
+++ www/templates/group/group.js
@@ -1,26 +1,40 @@
 angular.module('weDo.group', [])
-  // .factory('group', function () {
-  //   var g = {groups : [
-  //     {title: 'Family', des: "The McDaniel Family to-do list"},
-  //     {title: 'Soccer Team', des: "The community soccer team that Pete coaches."},
-  //     {title: 'Pot-Luck Dinner', des: 'A weekly pot-luck dinner among the neighborhood.'}
-  //     ]}
-  //   return g;
-  // })
 
-  .controller('GroupCtrl', function () {
-    // , ['$rootscope', 'groups',
-  //   function($rootscope, groups) {
-  //     $rootscope.groups = groups.groups;
-  //     // [
-  //     // {title: 'Family', des: "The McDaniel Family to-do list"},
-  //     // {title: 'Soccer Team', des: "The community soccer team that Pete coaches."},
-  //     // {title: 'Pot-Luck Dinner', des: 'A weekly pot-luck dinner among the neighborhood.'}
-  //     // ];
-  //     $rootscope.addGroup = function () {
-  //       // console.log($scope);
-  //       var newGroup = {title: $rootscope.title, des: $rootscope.des};
-  //       $rootscope.groups.push(newGroup);
-  //     };
-    })
+  .controller('GroupCtrl', function ($scope, $http) {
+    $scope.groupData = {};
 
+    // when landing on the page, get all todos and show them
+    $http.get('/api/groups')
+      .success(function(data) {
+        $scope.groups = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createGroup = function() {
+      $http.post('/api/groups', $scope.groupData)
+        .success(function(data) {
+          $scope.groupData = {}; // clear the form so our user is ready to enter another
+          $scope.groups = data;
+          console.log(data);
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteGroup = function(id) {
+      $http.delete('/api/groups/' + id)
+        .success(function(data) {
+          $scope.groups = data;
+          // console.log(data);
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
+  })
--- www/templates/register/register.js
+++ www/templates/register/register.js
@@ -1,4 +1,38 @@
 angular.module('weDo.register', [])
-  .controller('RegisterCtrl', function () {
+  .controller('RegisterCtrl', function ($scope, $http) {
+    $scope.userData = {};
 
+    // when landing on the page, get all todos and show them
+    // $http.get('/api/users')
+    //   .success(function(data) {
+    //     $scope.users = data;
+    //     // console.log(data);
+    //   })
+    //   .error(function(data) {
+    //     console.log('Error: ' + data);
+    //   });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createUser = function() {
+      $http.post('/api/users', $scope.userData)
+        .success(function(data) {
+          $scope.userData = {}; // clear the form so our user is ready to enter another
+          $scope.users = data;
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
+
+    // delete a todo after checking it
+    // $scope.deleteUser = function(id) {
+    //   $http.delete('/api/users/' + id)
+    //     .success(function(data) {
+    //       $scope.users = data;
+    //       // console.log(data);
+    //     })
+    //     .error(function(data) {
+    //       console.log('Error: ' + data);
+    //     });
+    // };
   })
